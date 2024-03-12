c901efa816743ef7da901b89c2c624c8958380b6
Игорь Некрашевич <misterpekc@gmail.com>
2024-03-12 09:36:51 +0700

    update manager

--- HomeworksStudent/SmartHome/HomeManager.cs
+++ HomeworksStudent/SmartHome/HomeManager.cs
@@ -4,12 +4,28 @@ namespace HomeworksStudent.SmartHome
 {
     public class HomeManager
     {
+        public static readonly HomeManager Instance;
+        private HomeManager()
+        { }
+
+        static HomeManager()
+        {
+            Instance = new HomeManager();
+        }
+
         Dictionary<Type, IAction> _keyValuePairs = new()
             {
                 { typeof(LightOn), new LightOn() },
                 { typeof(LightOff), new LightOff() },
                 { typeof(WaterStart), new WaterStart() }
             };
+
+        List<IAction> actions = new List<IAction>() {
+        new LightOn(),
+        new LightOff(),
+        new LightOn(),
+        }
+        ;
 
         public void StartAction<T>() where T : IAction
         {
--- HomeworksStudent/SmartHome/SmartHomeStarter.cs
+++ HomeworksStudent/SmartHome/SmartHomeStarter.cs
@@ -1,23 +1,282 @@
-﻿namespace HomeworksStudent.SmartHome
+﻿using HomeworksStudent.Homeworks.Sedishev.Shop;
+using HomeworksStudent.PersonAbstract.StringBuilders;
+using System.Text;
+
+namespace HomeworksStudent.SmartHome
 {
     public class SmartHomeStarter : IEntryPoint
     {
         public void Start()
         {
-            HomeManager homeManager = new HomeManager();
-            homeManager.StartAction<LightOff>();
+            HomeManager.Instance.StartAction<LightOff>();
             Thread.Sleep(2000);
-            homeManager.StartAction<LightOn>();
+            HomeManager.Instance.StartAction<LightOn>();
             Thread.Sleep(2000);
-            homeManager.StartAction<WaterStart>();
+            HomeManager.Instance.StartAction<WaterStart>();
+
+            //List<IComandd> listComand = new List<IComandd>();
+            //listComand.Add(new InputComand());
+            //listComand.Add(new CreateUserComand());
+
+            //StringBuilder stringBuilder = new StringBuilder();
+
+            //for (var i = 0; i < listComand.Count; i++)
+            //{
+            //    stringBuilder.AppendLine($"{i + 1} - {listComand[i].Description}");
+            //}
+
+            //while (true)
+            //{
+            //    if (InputHelper.Input(stringBuilder, 1, listComand.Count, out int inputValue))
+            //    {
+            //        listComand[inputValue - 1].Run();
+            //    }
+            //}
+        }
+    }
+
+    public interface IComandd
+    {
+        public string Description { get; }
+        public void Run();
+    }
+
+    public class FirstComand : IComandd
+    {
+        public string Description => throw new NotImplementedException();
+
+        public void Run()
+        {
+            Console.WriteLine("Эта реализация не похожа на вторую");
+        }
+    }
+
+    public class SecondComand : IComandd
+    {
+        public string Description => throw new NotImplementedException();
+
+        public void Run()
+        {
+            Console.WriteLine("Эта реализация не похожа на первую");
+        }
+    }
+
+    public class CreateUserComand : IComandd
+    {
+        public string Description => "Создать пользователя";
+
+        public void Run()
+        {
+            Console.WriteLine("Введите имя пользователя");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Введите пароль");
+            string userPassword = Console.ReadLine();
+            UserManager.Instance.AddUser(new User(userName, userPassword));
+        }
+    }
+
+    public class InputComand : IComandd
+    {
+        public string Description => "Вход в аккаунт";
+
+        public void Run()
+        {
+            Console.WriteLine("Введите имя пользователя");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Введите пароль");
+            string userPassword = Console.ReadLine();
+            if (UserManager.Instance.CheckUser(userName, userPassword))
+            {
+                Console.WriteLine("Вы вошли в приложение!");
+            }
+        }
+    }
+
+    public class UserManager
+    {
+        public static readonly UserManager Instance;
+        static UserManager()
+        {
+            Instance = new UserManager();
+        }
+        private UserManager()
+        { }
+
+        private List<User> _users = new List<User>();
+
+        public void AddUser(User user)
+        {
+            _users.Add(user);
+        }
+
+        public bool CheckUser(string userName, string password)
+        {
+            bool result = false;
+
+            foreach (var item in _users)
+            {
+                if (item.UserName == userName)
+                {
+                    result = item.PasswordIsTrue(password);
+                }
+            }
+            return result;
+        }
+    }
+
+    public class User
+    {
+        public readonly string UserName;
+        public readonly string Password;
+
+        public User(string userName, string password)
+        {
+            UserName = userName;
+            Password = password;
+        }
+
+        public bool PasswordIsTrue(string password)
+        {
+            return Password == password;
+        }
+    }
+
+    public class Cart
+    {
+        #region Singletone
+        public static readonly Cart Instance;
+        private Cart()
+        {
+
+        }
+        static Cart()
+        {
+            Instance = new Cart();
+        }
+        #endregion
+
+        private List<Product> _products = new();
+
+        public void AddProduct()
+        {
+            Console.WriteLine("Введите имя продукта!");
+            string productName = Console.ReadLine();
+            Console.WriteLine("Введите цену продукта");
+            if (int.TryParse(Console.ReadLine(), out int priceValue))
+            {
+                if (!string.IsNullOrEmpty(productName))
+                {
+                    _products.Add(new Product(productName, priceValue));
+                }
+            }
+        }
+
+        public void RemoveProduct()
+        {
+            if (!CheckContainUser())
+            {
+                return;
+            }
+
+            while (true)
+            {
+                if (InputHelper.Input(GetDescription(), 1, _products.Count, out int inputValue))
+                {
+                    _products.RemoveAt(inputValue - 1);
+                    break;
+                }
+            }
+        }
+
+        private StringBuilder GetDescription()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (var i = 0; i < _products.Count; i++)
+            {
+                Product? product = _products[i];
+                stringBuilder.AppendLine($"{i + 1} - {product.Name}");
+            }
+            return stringBuilder;
+        }
+
+        private bool CheckContainUser()
+        {
+            return _products.Count > 0;
+        }
+    }
+
+    public class AddComand : IAction, IDescription
+    {
+        public string Description => "Добавить продукт";
+
+        public void Run()
+        {
+            Cart.Instance.AddProduct();
+        }
+    }
+
+    public class RemoveComand : IAction, IDescription
+    {
+        public string Description => "Удаление продукта";
+
+        public void Run()
+        {
+            Cart.Instance.RemoveProduct();
+        }
+    }
+
+    public class SendComand : IAction, IDescription
+    {
+        public string Description => "Отправка запроса";
+
+        public void Run()
+        {
+            Console.WriteLine("Отправил запрос о продукте");
+        }
+    }
+
+    public class Product
+    {
+        public readonly string Name;
+        public readonly int Price;
+
+        public Product(string name, int price)
+        {
+            Price = price;
+            Name = name;
+        }
+    }
+
+    public class CartStarter : IEntryPoint
+    {
+        public void Start()
+        {
+            IAction[] actions = {
+                new SendComand(),
+                new RemoveComand(),
+                new AddComand()
+            };
+
+            StringBuilder elem = ActionHelper.GetDescriptionForAction(actions);
+
+            while (true)
+            {
+                if (InputHelper.Input(elem, 1, actions.Length, out int inputValue))
+                {
+                    actions[inputValue - 1].Run();
+                }
+            }
         }
     }
 }
 
+
+
 //Задача 1
 //Система управления заказами в интернет-магазине: 
 //Реализовать систему, которая позволяет пользователям добавлять товары в корзину, 
-//удалять их, изменять количество и отправлять заказ. 
+//удалять их, и отправлять заказ. 
 //Каждая операция (добавление, удаление, отправка заказа(Cw("Отправил"))) 
 //должна быть инкапсулирована в отдельный объект команды.
 //Корзина должна быть синглетоном.
@@ -32,3 +291,12 @@
 //Команда на заказ еды
 //Работа команд осуществляется через Console.WriteLine();
 //Написать Singletone менеджер, который запускает каждую команду по отдельности
+
+//Задача 3
+//Каждое действие - отдельная команда
+//Система управления пользователями есть возможность:
+//1 - Добавить пользователя.
+//2 - Удалить пользователя.
+//3 - Получить информацию о пользователях (Имя пользователя, фамилия пользователя, дата добавления пользователя)
+//Пользователи должны храниться в менеджере пользователей который содержит коллекцию этих пользователей и производит с ними вышеописанные действия
+
